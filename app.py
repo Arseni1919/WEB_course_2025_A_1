a9eecc72a5c388b5b885085e0fc08ce7cfebb50c
perchik_air <1919ars@gmail.com>
2025-01-15 17:01:33 +0200

    more

--- app.py
+++ app.py
@@ -143,17 +143,171 @@ def fetch_example_func():
     else:
         raise RuntimeError('')
 
+# ------------------------------------------------------------------ #
+# ------------------------------------------------------------------ #
+# ------------------------------------------------------------------ #
+# MONGODB
+# ------------------------------------------------------------------ #
+# ------------------------------------------------------------------ #
+# ------------------------------------------------------------------ #
+import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-# @app.route('/fetch_example', methods=['GET', 'POST'])
-# def fetch_example_func():
-#     if request.method == 'GET':
-#         data = {'message': 'GET response'}
-#         return json.dumps(data)
-#     if request.method == 'POST':
-#         mydict = request.json
-#         print(type(mydict))
-#         print(mydict)
-#         data = {'message': 'POST response'}
-#         data.update(mydict)
-#         return json.dumps(data)
-#     raise RuntimeError('no no')
+uri = "mongodb+srv://1919ars:1919ars@cluster0.kg52uoz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new cluster and connect to the server
+cluster = MongoClient(uri, server_api=ServerApi('1'))
+mydatabase = cluster['mydatabase']
+customers_col = mydatabase['customers']
+product_col = mydatabase['products']
+
+# sample_analytics_db = cluster['sample_analytics']
+
+
+@app.route('/mongodb')
+def mongodb_func():
+    # message = 'good'
+    # message = pymongo.version
+    # message = cluster.list_database_names()
+    # sample_mflix = cluster['sample_mflix']
+    # message = sample_mflix.list_collection_names()
+
+    # insert_one
+    # take the data from form / from FE
+    # my_dict = {
+    #     'name': 'Ortal',
+    #     'address': 'Aba Hillel 100',
+    #     'rating': 19
+    # }
+    # customers_col.insert_one(my_dict)
+    # # # message = cluster.list_database_names()
+    # message = my_dict
+
+    # insert_many
+    # my_list = [
+    #     {'name': 'Tal', 'address': 'Hogwards 37', 'rating': 11},
+    #     {'name': 'Bekka', 'address': 'Bronx 3', 'rating': 20},
+    #     {'name': 'Alisa', 'address': 'Area 9', 'rating': 30},
+    # ]
+    # customers_col.insert_many(my_list)
+    # message = my_list
+
+    # findOne
+    # message = customers_col.find_one({'name': 'John'})
+
+    # find
+    # message = list(customers_col.find())
+
+    # find query
+    # myquery = {'name': 'John'}
+    # message = list(customers_col.find(myquery))
+    # myquery = {'rating': {"$gt": 10}}
+    # message = list(customers_col.find(myquery))
+
+    # sort
+    # myquery = {'rating': {"$gt": 10}}
+    # message = list(customers_col.find().sort('name'))
+    # message = list(customers_col.find(myquery).sort('name', -1))
+    # message = len(list(customers_col.find()))
+
+    # limit
+    # message = len(list(customers_col.find()))
+    # myquery = {'rating': {"$lt": 50}}
+    # message = list(customers_col.find(myquery).sort('rating', -1).limit(3))
+    # message = list(customers_col.find().sort('rating', -1).limit(3))
+
+    # update one
+    # my_query = {'address': 'Highway 37'}
+    # new_values = {'$set': {'address': 'Canyon 123'}}
+    # customers_col.update_one(my_query, new_values)
+    # message = list(customers_col.find({'address': 'Canyon 123'}))
+
+    # update many
+    # customers_col.update_many({}, {'$inc': {'rating': 1}})
+    # customers_col.update_many({}, {'$set': {'rating': 100}})
+    # message = list(customers_col.find())
+
+    # delete one
+    # customers_col.delete_one({'name': 'Ars Perchik'})
+    # message = list(customers_col.find())
+
+    # delete many
+    # customers_col.delete_many({'rating': {'$gt': 1000}})
+    # customers_col.delete_many({'name': 'Tal'})
+    # message = list(customers_col.find())
+
+    # aggregations
+    # aggregation = [
+    #     {
+    #         '$match': {
+    #             'rating': 100
+    #         }
+    #     }, {
+    #         '$sort': {
+    #             'name': -1
+    #         }
+    #     }, {
+    #         '$limit': 5
+    #     }
+    # ]
+    # message = list(customers_col.aggregate(aggregation))
+    # message = customers_col.find()
+    message = ''
+    # return render_template('mongodb_lecture.html', message=message)
+
+    my_list = customers_col.find()
+    return render_template('mongodb_lecture.html', my_list=my_list)
+
+
+
+
+
+
+
+
+@app.route('/db_insert')
+def insert_func():
+    # insert_one
+    my_dict = {
+        'name': request.args['name'],
+        'address': request.args['address'],
+        'rating': int(request.args['rating']),
+    }
+    customers_col.insert_one(my_dict)
+    return redirect(url_for('mongodb_func'))
+
+
+
+
+
+
+
+
+
+@app.route('/db_delete', methods=['POST'])
+def delete_func():
+    print(request.form)
+    customers_col.delete_one({'name': request.form['name']})
+    return redirect(url_for('mongodb_func'))
+
+
+
+
+
+
+
+
+
+
+@app.route('/db_increment')
+def increment_func():
+    customers_col.update_many({}, {'$inc': {'rating': 1}})
+    return redirect(url_for('mongodb_func'))
+
+
+
+
+
+
+
